96be49c58290b2428de3b9c9aa298596c35ddfa2
hirwaubaine <hirwaubaine@gmail.com>
2023-04-20 04:33:14 -0700

    task number 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A C program that prints a line by using printf not puts
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
