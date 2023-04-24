c909c8e8169d7c058d339b463c5f996a96602822
hirwaubaine <hirwaubaine@gmail.com>
2023-04-24 08:10:05 -0700

    task number 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all the numbers of base 16 in lowercase,
+ * Return:0
+ */
+int main(void)
+{
+	int num;
+	char l;
+
+	for (num = 0; num <= 10; num++)
+		putchar((num % 10) + '0');
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
