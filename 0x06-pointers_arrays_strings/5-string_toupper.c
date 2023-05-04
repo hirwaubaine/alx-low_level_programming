8eef874887fe27002a5e0ea9d467fded54cff7a6
hirwaubaine <hirwaubaine@gmail.com>
2023-05-04 06:34:52 -0700

    task number 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 /**
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
- * Return: 0
+ * Return: n
  */
 char *string_toupper(char *n)
 {
@@ -15,5 +15,5 @@ char *string_toupper(char *n)
 			n[i] = n[i] - 32;
 		i++;
 	}
-	return (0);
+	return (n);
 }
