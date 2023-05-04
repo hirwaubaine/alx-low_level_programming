e262ca1de3588e6d01c37f3ab05f94ebe4375332
hirwaubaine <hirwaubaine@gmail.com>
2023-05-04 06:32:47 -0700

    task number 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * @dest: input value
  * @src: input value
  * @n: input value
- * Return:0
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -22,5 +22,5 @@ char *_strncpy(char *dest, char *src, int n)
 		j++;
 	}
 
-	return (0);
+	return (dest);
 }
