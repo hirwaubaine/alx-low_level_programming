1ccb0e8d01f5895f8ed3c1c2751a970a952612b1
hirwaubaine <hirwaubaine@gmail.com>
2023-05-24 03:16:25 -0700

    tasks 0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_n - print name using pointer to function
- * @n: string to add
+ * print_name - print name using pointer to function
+ * @name: string to add
  * @f: pointer to function
  * Return: nothing
  **/
-void print_n(char *n, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-	if (n == NULL || f == NULL)
+	if (name == NULL || f == NULL)
 		return;
 
-	f(n);
+	f(name);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 /**
- * int_ind - return index place if comparison = true, else -1
+ * int_index - return index place if comparison = true, else -1
  * @array: array
  * @size: size of elements in an array
  * @cmp: pointer to function of one of the 3 in main
  * Return: 0
  */
-int int_ind(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
