28e7b6f854e380ba4ef1b5a0bb31a8cff99de754
hirwaubaine <hirwaubaine@gmail.com>
2023-06-01 05:10:26 -0700

    ALX tasks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head: a head pointer of the list
+ * @str: a string to add on the list
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int len;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	for (len = 0; str[len]; len++)
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
