3f2aeed3b738f0e4e90fef85b3c7b66b9cb88845
hirwaubaine <hirwaubaine@gmail.com>
2023-06-05 12:42:54 -0700

    ALX task

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a linked list
+ * @head: pointer to the very first node of the list
+ * Return: the head node’s data (n)
+ * or 0 if the linked list is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (head == NULL)
+		return (0);
+
+	temp = *head;
+	*head = temp->next;
+	n = temp->n;
+	free(temp);
+	return (n);
+}
