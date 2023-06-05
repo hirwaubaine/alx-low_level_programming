bc2dda34d3a65bc5acf3f35bae39fdaf5c19fbcc
hirwaubaine <hirwaubaine@gmail.com>
2023-06-05 14:29:34 -0700

    ALX task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,7 +7,7 @@
  * @head: pointer to the very first node of the list
  * @idx: index of the list where the new node should be added
  * @n: integer to be used for adding new node
- * Returns: the address of the new node, or NULL if it failed
+ * Return: pointer of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
