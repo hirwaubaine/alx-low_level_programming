e39dbfc356775b0cb6b2c6cfce2ec64fc1fba62d
hirwaubaine <hirwaubaine@gmail.com>
2023-06-05 13:07:07 -0700

    ALX task

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a linked list
+ * @head: pointer to the very first node of the list
+ * @index: index of the node
+ * Return: pointer of the pointed node
+ * or NULL, if the node does not exist
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t n;
+
+	while (n < index)
+	{
+		head = head->next;
+		n++;
+	}
+	return (head ? head : NULL);
+}
