0d5bc2fc75863bcb767c2250b5d98c72f7882b8c
hirwaubaine <hirwaubaine@gmail.com>
2023-06-05 11:27:01 -0700

    ALX task

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,13 +11,16 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	 listint_t *newnode;
+	listint_t *newnode;
 
-	 newnode = malloc(sizeof(listint_t));
-	 if (newnode == NULL)
-		 return (NULL);
-	 newnode->n = n;
-	 newnode->next = *head;
-	 *head = newnode;
-	 return (newnode);
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	else
+		newnode->n = n;
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
 }
