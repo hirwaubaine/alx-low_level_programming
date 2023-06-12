f9ccf3f54ef1028b8936e5a7c4a7e2bc507ced4a
hirwaubaine <hirwaubaine@gmail.com>
2023-06-12 12:22:36 -0700

    1 task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, n, w = 0;
+	int fd, n, w;
 
 	if (filename == NULL)
 		return (-1);
@@ -18,9 +18,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
+		text_content = "";
 	{
-		for (n = 0; text_content[n]; n++)
-			;
+		for (n = 0; text_content[n];)
+			n++;
 	}
 	w = write(fd, text_content, n);
 
